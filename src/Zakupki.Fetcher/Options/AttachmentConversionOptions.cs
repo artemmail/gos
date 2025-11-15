7ca3cb039ca0170e5b696faad464c25e084b6994
artemmail <artemmail@gmail.com>
2025-11-15 09:10:05 +0300

    Support legacy doc attachments via LibreOffice conversion

--- src/Zakupki.Fetcher/Options/AttachmentConversionOptions.cs
+++ src/Zakupki.Fetcher/Options/AttachmentConversionOptions.cs
@@ -5,4 +5,6 @@ public class AttachmentConversionOptions
     public string? PandocPath { get; set; }
 
     public string? PandocWorkingDirectory { get; set; }
+
+    public string? LibreOfficePath { get; set; }
 }
