1012c4a64e24c0adf681c9dadb127cc024fdc02b
artemmail <artemmail@gmail.com>
2025-12-05 14:12:32 +0300

    Fix DateTime converters to use System types

--- src/Zakupki.MosApi/Serialization/IsoDateTimeConverter.cs
+++ src/Zakupki.MosApi/Serialization/IsoDateTimeConverter.cs
@@ -2,40 +2,41 @@ using System;
 using System.Globalization;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using SystemDateTime = global::System.DateTime;
 
 namespace Zakupki.MosApi.Serialization;
 
-public class IsoDateTimeConverter : JsonConverter<DateTime>
+public class IsoDateTimeConverter : JsonConverter<SystemDateTime>
 {
     private const string Format = "yyyy-MM-dd'T'HH:mm:ss";
 
-    public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    public override SystemDateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
         var value = reader.GetString();
         return value is null
             ? default
-            : DateTime.Parse(value, CultureInfo.InvariantCulture, DateTimeStyles.RoundtripKind);
+            : SystemDateTime.Parse(value, CultureInfo.InvariantCulture, DateTimeStyles.RoundtripKind);
     }
 
-    public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+    public override void Write(Utf8JsonWriter writer, SystemDateTime value, JsonSerializerOptions options)
     {
         writer.WriteStringValue(value.ToString(Format, CultureInfo.InvariantCulture));
     }
 }
 
-public class IsoDateTimeNullableConverter : JsonConverter<DateTime?>
+public class IsoDateTimeNullableConverter : JsonConverter<SystemDateTime?>
 {
     private const string Format = "yyyy-MM-dd'T'HH:mm:ss";
 
-    public override DateTime? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    public override SystemDateTime? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
         var value = reader.GetString();
         return string.IsNullOrEmpty(value)
             ? null
-            : DateTime.Parse(value, CultureInfo.InvariantCulture, DateTimeStyles.RoundtripKind);
+            : SystemDateTime.Parse(value, CultureInfo.InvariantCulture, DateTimeStyles.RoundtripKind);
     }
 
-    public override void Write(Utf8JsonWriter writer, DateTime? value, JsonSerializerOptions options)
+    public override void Write(Utf8JsonWriter writer, SystemDateTime? value, JsonSerializerOptions options)
     {
         if (value.HasValue)
         {
